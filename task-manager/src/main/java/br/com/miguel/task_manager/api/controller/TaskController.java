b6b68fdd782910db30d29252e9f36c549a208ee9
Miguel-Frnds <miguel.pereiraf05@gmail.com>
2026-03-13 18:06:06 -0300

    Adiciona validações em UserController e TaskController

--- task-manager/src/main/java/br/com/miguel/task_manager/api/controller/TaskController.java
+++ task-manager/src/main/java/br/com/miguel/task_manager/api/controller/TaskController.java
@@ -1,43 +1,49 @@
 package br.com.miguel.task_manager.api.controller;
 
-import br.com.miguel.task_manager.api.dto.TaskRequestDTO;
-import br.com.miguel.task_manager.api.dto.TaskResponseDTO;
+import br.com.miguel.task_manager.api.dto.task.TaskRequestDTO;
+import br.com.miguel.task_manager.api.dto.task.TaskResponseDTO;
+import br.com.miguel.task_manager.api.dto.task.TaskUpdateDTO;
 import br.com.miguel.task_manager.domain.service.TaskService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/users/{userId}/tasks")
+@Validated
 public class TaskController {
 
     @Autowired
     private TaskService taskService;
 
     @GetMapping()
-    public ResponseEntity<List<TaskResponseDTO>> findAll(@PathVariable("userId") Long userId) {
+    public ResponseEntity<List<TaskResponseDTO>> findAll(@PathVariable("userId") @NotNull @Positive Long userId) {
         List<TaskResponseDTO> tasks = taskService.findAll(userId);
         return ResponseEntity.ok().body(tasks);
     }
 
     @PostMapping()
-    public ResponseEntity<TaskResponseDTO> save(@PathVariable("userId") Long userId, @RequestBody TaskRequestDTO task) {
+    public ResponseEntity<TaskResponseDTO> save(@PathVariable("userId") @NotNull @Positive Long userId, @Valid @RequestBody TaskRequestDTO task) {
         TaskResponseDTO newTask = taskService.save(userId, task);
         return ResponseEntity.status(201).body(newTask);
     }
 
     @PatchMapping("{taskId}")
-    public ResponseEntity<TaskResponseDTO> update(@PathVariable("userId") Long userId,
-                                       @PathVariable("taskId") Long taskId,
-                                       @RequestBody TaskRequestDTO task) {
+    public ResponseEntity<TaskResponseDTO> update(@PathVariable("userId") @NotNull @Positive Long userId,
+                                       @PathVariable("taskId") @NotNull @Positive Long taskId,
+                                       @Valid @RequestBody TaskUpdateDTO task) {
         TaskResponseDTO updateTask = taskService.update(userId, taskId, task);
         return ResponseEntity.ok().body(updateTask);
     }
 
     @DeleteMapping("{taskId}")
-    public ResponseEntity<Void> delete(@PathVariable("userId") Long userId, @PathVariable("taskId") Long taskId) {
+    public ResponseEntity<Void> delete(@PathVariable("userId") @NotNull @Positive Long userId, @PathVariable("taskId") @NotNull @Positive Long taskId) {
         taskService.delete(userId, taskId);
         return ResponseEntity.noContent().build();
     }
--- task-manager/src/main/java/br/com/miguel/task_manager/api/controller/UserController.java
+++ task-manager/src/main/java/br/com/miguel/task_manager/api/controller/UserController.java
@@ -1,16 +1,22 @@
 package br.com.miguel.task_manager.api.controller;
 
-import br.com.miguel.task_manager.api.dto.UserRequestDTO;
-import br.com.miguel.task_manager.api.dto.UserResponseDTO;
+import br.com.miguel.task_manager.api.dto.user.UserRequestDTO;
+import br.com.miguel.task_manager.api.dto.user.UserResponseDTO;
+import br.com.miguel.task_manager.api.dto.user.UserUpdateDTO;
 import br.com.miguel.task_manager.domain.service.UserService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
+@Validated
 public class UserController {
 
     @Autowired
@@ -23,25 +29,25 @@ public class UserController {
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<UserResponseDTO> findById(@PathVariable("id") Long id){
+    public ResponseEntity<UserResponseDTO> findById(@PathVariable("id") @NotNull @Positive Long id){
         UserResponseDTO user = userService.findUserById(id);
         return ResponseEntity.ok().body(user);
     }
 
     @PostMapping
-    public ResponseEntity<UserResponseDTO> save(@RequestBody UserRequestDTO user){
+    public ResponseEntity<UserResponseDTO> save(@Valid @RequestBody UserRequestDTO user){
         UserResponseDTO newUser = userService.save(user);
         return ResponseEntity.status(201).body(newUser);
     }
 
     @PatchMapping("{id}")
-    public ResponseEntity<UserResponseDTO> update(@PathVariable("id") Long id, @RequestBody UserRequestDTO user){
+    public ResponseEntity<UserResponseDTO> update(@PathVariable("id") @NotNull  @Positive Long id, @Valid @RequestBody UserUpdateDTO user){
         UserResponseDTO newUser = userService.update(id, user);
         return ResponseEntity.ok().body(newUser);
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<Void> delete(@PathVariable("id") Long id){
+    public ResponseEntity<Void> delete(@PathVariable("id") @NotNull @Positive Long id){
         userService.delete(id);
         return ResponseEntity.noContent().build();
     }
