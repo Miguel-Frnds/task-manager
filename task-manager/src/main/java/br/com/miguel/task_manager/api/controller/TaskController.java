971740ee7d7ebab1098e1395cb0ef078594bb41a
Miguel-Frnds <miguel.pereiraf05@gmail.com>
2026-03-14 16:04:07 -0300

    Adiciona paginação e filtragem de Tasks

--- task-manager/src/main/java/br/com/miguel/task_manager/api/controller/TaskController.java
+++ task-manager/src/main/java/br/com/miguel/task_manager/api/controller/TaskController.java
@@ -1,18 +1,21 @@
 package br.com.miguel.task_manager.api.controller;
 
+import br.com.miguel.task_manager.api.dto.PageDTO;
 import br.com.miguel.task_manager.api.dto.task.TaskRequestDTO;
 import br.com.miguel.task_manager.api.dto.task.TaskResponseDTO;
 import br.com.miguel.task_manager.api.dto.task.TaskUpdateDTO;
+import br.com.miguel.task_manager.domain.entity.Priority;
+import br.com.miguel.task_manager.domain.entity.Status;
 import br.com.miguel.task_manager.domain.service.TaskService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/users/{userId}/tasks")
@@ -23,8 +26,11 @@ public class TaskController {
     private TaskService taskService;
 
     @GetMapping()
-    public ResponseEntity<List<TaskResponseDTO>> findAll(@PathVariable("userId") @NotNull @Positive Long userId) {
-        List<TaskResponseDTO> tasks = taskService.findAll(userId);
+    public ResponseEntity<PageDTO<TaskResponseDTO>> findAllTasks(@PageableDefault(size = 10, sort = "title") Pageable pageable,
+                                                                 @PathVariable("userId") @NotNull @Positive Long userId,
+                                                                 @RequestParam(value = "status", required = false) Status status,
+                                                                 @RequestParam(value = "priority", required = false) Priority priority) {
+        PageDTO<TaskResponseDTO> tasks = taskService.findAllTasks(userId, status, priority, pageable);
         return ResponseEntity.ok().body(tasks);
     }
 
