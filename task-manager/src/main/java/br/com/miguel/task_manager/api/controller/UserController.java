bb5d8c1449027992c7ad761fea9be286cecaf70e
Miguel-Frnds <miguel.pereiraf05@gmail.com>
2026-03-14 16:04:35 -0300

    Adiciona paginação de Users

--- task-manager/src/main/java/br/com/miguel/task_manager/api/controller/UserController.java
+++ task-manager/src/main/java/br/com/miguel/task_manager/api/controller/UserController.java
@@ -1,5 +1,6 @@
 package br.com.miguel.task_manager.api.controller;
 
+import br.com.miguel.task_manager.api.dto.PageDTO;
 import br.com.miguel.task_manager.api.dto.user.UserRequestDTO;
 import br.com.miguel.task_manager.api.dto.user.UserResponseDTO;
 import br.com.miguel.task_manager.api.dto.user.UserUpdateDTO;
@@ -8,11 +9,11 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -23,9 +24,8 @@ public class UserController {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<UserResponseDTO>> findAll(){
-        List<UserResponseDTO> users = userService.findAll();
-        return ResponseEntity.ok().body(users);
+    public ResponseEntity<PageDTO<UserResponseDTO>> findAll(@PageableDefault(size = 10, sort = "username") Pageable pageable){
+        return ResponseEntity.ok().body(userService.findAll(pageable));
     }
 
     @GetMapping("{id}")
