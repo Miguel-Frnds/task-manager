0dea20fbf793e951c16c52ae0f0ba286fb00f712
Miguel-Frnds <miguel.pereiraf05@gmail.com>
2026-03-14 16:01:33 -0300

    Adiciona filtros de Status e Priority com paginação

--- task-manager/src/main/java/br/com/miguel/task_manager/domain/repository/TaskRepository.java
+++ task-manager/src/main/java/br/com/miguel/task_manager/domain/repository/TaskRepository.java
@@ -1,14 +1,20 @@
 package br.com.miguel.task_manager.domain.repository;
 
+import br.com.miguel.task_manager.domain.entity.Priority;
+import br.com.miguel.task_manager.domain.entity.Status;
 import br.com.miguel.task_manager.domain.entity.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    List<Task> findByUserId(Long userId);
+    Page<Task> findByUserId(Long userId, Pageable pageable);
     Optional<Task> findByIdAndUserId(Long taskId, Long userId);
+    Page<Task> findByUserIdAndStatus(Long userId, Status status, Pageable pageable);
+    Page<Task> findByUserIdAndPriority(Long userId, Priority priority, Pageable pageable);
+    Page<Task> findByUserIdAndStatusAndPriority(Long userId, Status status, Priority priority, Pageable pageable);
 }
