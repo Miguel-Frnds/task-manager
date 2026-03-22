5d623a732af54eb18201fc25cb0bd8f9034a60d2
Miguel-Frnds <miguel.pereiraf05@gmail.com>
2026-03-22 18:26:51 -0300

    Adiciona configuração de segurança JWT com filtro e geração de token

---
+++ task-manager/src/main/java/br/com/miguel/task_manager/infra/security/JwtAuthenticationFilter.java
@@ -0,0 +1,44 @@
+package br.com.miguel.task_manager.infra.security;
+
+import br.com.miguel.task_manager.domain.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.extractTokenFromHeader(request);
+        if(token != null){
+            var email = tokenService.validateToken(token);
+            UserDetails user = userDetailsService.loadUserByUsername(email);
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String extractTokenFromHeader(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+}
---
+++ task-manager/src/main/java/br/com/miguel/task_manager/infra/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package br.com.miguel.task_manager.infra.security;
+
+import br.com.miguel.task_manager.domain.entity.User;
+import br.com.miguel.task_manager.domain.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = repository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("Email não encontrado");
+        }
+        return user;
+    }
+}
